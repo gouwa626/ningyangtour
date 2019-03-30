8c5eff8a636efd7d6c1f202c1ea26a0582ac40b4
xibaoqiang <18518009180@163.com>
2019-03-30 21:44:06 +0800

    文物页面

---
+++ src/base/Backbar/index.vue
@@ -0,0 +1,69 @@
+<template>
+   <div class="backWraper">
+     <div class="left">
+       <i>C</i>
+       <span>返回</span>
+     </div>
+     <div class="center">
+       {{title}}
+     </div>
+     <div class="right">
+       ...
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'Backbar',
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted() {
+
+  },
+  watch: {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.backWraper{
+  // position:fixed;
+  // top:0;
+  // left: 0;
+  width: 100%;
+  height: 44px;
+  background: #2b2b2b;
+  display:flex;
+  align-items: center;
+  color: #fff;
+  font-size: 16px;
+}
+  .left{
+    width: 20%;
+    }
+  .center{
+    flex:1;
+    text-align: center;
+  }
+  .right{
+    width: 20%;
+    text-align: right;
+  }
+</style>
--- src/components/Wenwu/index.vue
+++ src/components/Wenwu/index.vue
@@ -1,10 +1,17 @@
 <template>
    <div>
-Wenwu
+     <Backbar title="文物"></Backbar>
+     <div class="subWraper">
+       <div class="items" v-for="(item,index) of 10" :key="index">
+         <img src="../../assets/logo.png">
+         <span class="text">前家庄遗址</span>
+       </div>
+     </div>
    </div>
 </template>
 
 <script>
+import Backbar from '@/base/Backbar'
 export default {
   name: 'Wenwu',
   data () {
@@ -22,11 +29,27 @@ export default {
 
   },
   components: {
-
+    Backbar
   }
 }
 </script>
 
 <style scoped lang='less'>
-
+.subWraper{
+  display: flex;
+  flex-wrap: wrap;
+  background: #fff;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.items{
+  width: 49%;
+  text-align: center;
+}
+.items img{
+  width: 100%;
+}
+.items:nth-child(odd){
+  margin-right: 2%;
+}
 </style>
