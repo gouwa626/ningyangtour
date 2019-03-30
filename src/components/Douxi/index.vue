61baa5353095864193abe8ebed43e29027d11ace
刘帆 <>
2019-03-30 21:34:37 +0800

    斗蟋页面

--- src/components/Douxi/index.vue
+++ src/components/Douxi/index.vue
@@ -1,6 +1,11 @@
 <template>
    <div>
-Douxi
+     <div v-for="(item,index) in 5" :key="index" class="douxi-wrap">
+       <img src="../../assets/logo.png" height="200" >
+       <p class="title-douxi">
+         2016年蟋蟀世界杯
+       </p>
+     </div>
    </div>
 </template>
 
@@ -26,7 +31,13 @@ export default {
   }
 }
 </script>
-
 <style scoped lang='less'>
+  .douxi-wrap{
+    padding: 12px 20px 3px;
 
+    .title-douxi {
+      text-align: center;
+    }
+  }
+  
 </style>
