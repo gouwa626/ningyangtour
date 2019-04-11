96ff1fe068cfef4d15e5cb5b790d12613587b269
刘帆 <>
2019-04-11 21:02:24 +0800

    app 高为100%

---
+++ src/components/BaoMing/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="signup-wrap">
+        <div>
+        </div>
+        <div>   
+
+        </div>
+        <div class="singup-footer"> 
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .signup-wrap {
+        height: 100%;
+        display: flex;
+
+        .singup-footer {
+            height: 125px;
+            width: 100%;
+            align-self: flex-end;
+            background-color: red;
+        }
+    }
+</style>
+
+
